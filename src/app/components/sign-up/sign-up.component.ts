0d2e7066dcad06146efbacabb16b3a55bf94a6bf
luis.porto <luis.porto.luis@gmail.com>
2024-07-14 17:17:43 -0300

    login token

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import { NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http'; 
+import { LoginResponse } from '../../types/login-response.type';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'sign-up',
   standalone: true,
-  imports: [MatIconModule,NgOptimizedImage],
+  imports: [MatIconModule,NgOptimizedImage,FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  formData: any = {};
+
+  constructor(private http: HttpClient) {}
+
+  submitForm() {
+    this.http.post<LoginResponse>('http://localhost:8080/v1/signIn', this.formData).subscribe(
+      response => {
+        console.log('Sucesso!', response);
+        sessionStorage.setItem("auth-token",response.token)
+      },
+      error => {
+        console.error('Erro ao enviar formulário:', error);
+        // Aqui você pode lidar com o erro, por exemplo, exibir uma mensagem de erro ao usuário.
+      }
+    )
+  }
 
 }
